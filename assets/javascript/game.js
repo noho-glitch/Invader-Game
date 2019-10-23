169845333d74feedcf84179191fa2311b2aed15b
Dave Nolan <dpn.nolan@gmail.com>
2019-10-23 07:42:46 -0400

    added target number generator

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,3 +9,9 @@ $("#cyan").on("click", function() {
     alert("test");
 
 });
+
+$(document).ready(function() {
+    var random = Math.floor(Math.random() * 100) + 19;
+    $("#targetNumber").text(random);
+    console.log(random)
+});
