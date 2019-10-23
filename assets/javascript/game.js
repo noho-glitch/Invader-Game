094446850ca1fc7708e8439b45b4c928eb5fe03a
Dave Nolan <dpn.nolan@gmail.com>
2019-10-23 15:23:20 -0400

    each button has its own value and score is updating

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,24 +1,74 @@
 var score = 0;
+var wins = 0;
+var losses = 0;
+var increment = Math.floor(Math.random() * 12) + 1;
 
-$(document).ready(function(){
-$(".space-invaders").on("click", function() {
+var purple1 = Math.floor(Math.random() * 12) + 1;
+var cyan1 = Math.floor(Math.random() * 12) + 1;
+var red1 = Math.floor(Math.random() * 12) + 1;
+var green1 = Math.floor(Math.random() * 12) + 1;
 
-    alert("test");
+
+$(document).ready(function () {
+    $("#score").text(score);
+
+    $("#cyan").on("click", function () {
+        // score += increment;
+        // alert("New score: " + score);
+        console.log("Cyan")
+        console.log(cyan1)
+        score = score + cyan1
+        console.log(score)
+        $("#score").text(score);
+
     });
-});
 
-$(document).ready(function(){
-$("#cyan").on("click", function() {
+    $("#red").on("click", function () {
+        // score += increment;
+        // alert("New score: " + score);
+        console.log("Red")
+        console.log(red1)
+        score += red1;
+        $("#score").text(score);
 
-    alert("test");
     });
-});
 
-$(document).ready(function() {
+    $("#purple").on("click", function () {
+        // score += increment;
+        // alert("New score: " + score);
+        console.log("purple")
+        console.log(purple1)
+        score += purple1;
+        $("#score").text(score);
+
+    });
+
+    $("#green").on("click", function () {
+        // score += increment;
+        // alert("New score: " + score);
+        console.log("green")
+        console.log(green1)
+        score += green1;
+        $("#score").text(score);
+    });
+
+
     var random = Math.floor(Math.random() * 100) + 19;
     $("#targetNumber").text(random);
-    console.log(random)
+
+    if (score === random) {
+        alert("You Win!")
+        wins++;
+        console.log("wins: " + wins);
+    }
+
+    else if (score >= random) {
+        losses++;
+        console.log("losses: " + losses);
+    }
+
+
 });
-$(document).ready(function(){
-    $("#score").text(score);
-});
+
+
+
